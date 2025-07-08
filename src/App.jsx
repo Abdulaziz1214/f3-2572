ce0560763cf3615198c0c0ab484496f64074ce20
Yusuf <sultvnhuzaevusufhuza@gmail.com>
2025-07-08 19:20:55 +0500

    yusuf

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import YusufKrasavchik from './components/YusufKrasavchik'
 
 const App = () => {
   return (
@@ -6,6 +7,7 @@ const App = () => {
       <h1>Echkilar</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex tempore obcaecati vel perferendis modi iusto culpa voluptates debitis ducimus deserunt distinctio, nesciunt soluta enim iste saepe harum libero, laborum porro.</p>
       <h3>Yusuf</h3>
+      <YusufKrasavchik />
     </div>
   )
 }
