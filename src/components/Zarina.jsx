53662ace72bfd70749e01876584a7149bf4e79b8
OsamuChuuya <osamuchuuya26@gmail.com>
2025-07-08 17:23:00 +0300

    ddd

---
+++ src/components/Zarina.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Zarina = () => {
+  return (
+    <div>
+        <h1>Zarina</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex tempore obcaecati vel perferendis modi iusto culpa voluptates debitis ducimus deserunt distinctio, nesciunt soluta enim iste saepe harum libero, laborum porro.</p>
+        <h3>Yusuf</h3>
+    </div>
+  )
+}
+
+export default Zarina
